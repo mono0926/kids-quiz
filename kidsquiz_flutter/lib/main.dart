cc023228af8c1f454a718a9664cf9fb10d1bea51
Masayuki Ono <mono0926@gmail.com>
2022-04-26 11:33:30 +0900

    GoRouter.setUrlPathStrategy(UrlPathStrategy.path);

--- kidsquiz_flutter/lib/main.dart
+++ kidsquiz_flutter/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
